5ab0f363da7174832befbe2e0b62b6c386a3d6e0
Oleg Denisov <nearxjob@yandex.ru>
2024-02-13 21:26:18 +0300

    PR EXAMPLE

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from "react";
+import UserList from "./components/UserList";
 
 function App() {
-  return (
-    <div className="App">
-      <></>
-    </div>
-  );
+    const [isShowUserList, setIsShowUserList] = useState(true);
+
+    return (
+        <div className="App">
+            <UserList enabled={isShowUserList} setIsShowUserList={setIsShowUserList} enableEscapeExit />
+            {/** Кнопка чтобы показать список пользователей к награде */}
+            <button onClick={() => setIsShowUserList(true)}>Показать пользователей к награде</button>
+        </div>
+    );
 }
 
 export default App;
